9d322d8da7b97009c59656be64663f7e60df97aa
John Isaac Delgado <ji23delgado@gmail.com>
2021-01-18 09:34:06 +0800

    finalize for defense

--- src/bankingappsimulation/bankGUI.java
+++ src/bankingappsimulation/bankGUI.java
@@ -7,9 +7,10 @@ import java.awt.event.*;
 public class bankGUI extends JFrame implements ActionListener {
 	
     main_buttons bal_button, withdraw, prev_transac_button, deposit, quit, back;
-    JTextField curr_bal;
+    JTextField curr_bal, lt_status;
     JLabel curr_bal_label;
-    keypad pad;
+    keypad pad, w_pad;
+    JPanel lt_panel;
 
     public bankGUI(){
 
@@ -74,8 +75,29 @@ public class bankGUI extends JFrame implements ActionListener {
         curr_bal.setFont(new Font("Arial", Font.PLAIN, 20));
 
         // make a j-panel for depositing
-        pad = new keypad(this.back);
+        pad = new keypad(this.back, "deposit");
         pad.setVisible(false);
+
+        // make a j-panel for withdrawing
+        w_pad = new keypad(this.back, "withdraw");
+        w_pad.setVisible(false);
+
+        // make a j-panel for the last previous transaction
+        lt_panel = new JPanel();
+        lt_panel.setBounds(250, 100, 200, 200);
+        lt_panel.setVisible(false);
+        lt_panel.setLayout(null);
+        lt_panel.setBackground(new Color(92, 219, 149));
+        JLabel lt_text = new JLabel();
+        lt_text.setText("Previous Transaction:");
+        lt_text.setBounds(10, 10, 150, 70);
+        lt_text.setFont(new Font("Arial", Font.PLAIN, 15));
+        lt_status = new JTextField();
+        lt_status.setBounds(0, 100, 200, 60);
+        lt_status.setFont(new Font("Arial", Font.PLAIN, 15));
+        lt_status.setEditable(false);
+        lt_panel.add(lt_text);
+        lt_panel.add(lt_status);
 
 
         // set window display settings
@@ -99,11 +121,13 @@ public class bankGUI extends JFrame implements ActionListener {
         button_cont.add(curr_bal_label);
         button_cont.add(back);
         button_cont.add(pad);
+        button_cont.add(w_pad);
+        button_cont.add(lt_panel);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
+            
         bal_button.setVisible(false);
         withdraw.setVisible(false);
         deposit.setVisible(false);
@@ -121,22 +145,75 @@ public class bankGUI extends JFrame implements ActionListener {
 
             pad.field_text = "";
             pad.deposit_field.setText(pad.field_text);
+            pad.one_point = false;
+
+            w_pad.field_text = "";
+            w_pad.deposit_field.setText(w_pad.field_text);
+            w_pad.one_point = false;
+            w_pad.error_msg.setVisible(false);
 
             bal_button.setVisible(true);
             withdraw.setVisible(true);
             deposit.setVisible(true);
             prev_transac_button.setVisible(true);
             quit.setVisible(true);
+
+
             back.setVisible(false);
             curr_bal.setVisible(false);
             curr_bal_label.setVisible(false);
             pad.setVisible(false);
+            w_pad.setVisible(false);
+            lt_panel.setVisible(false);
+
+            w_pad.btn0.setVisible(true);
+            w_pad.btn9.setVisible(true);
+            w_pad.btn8.setVisible(true);
+            w_pad.btn7.setVisible(true);
+            w_pad.btn6.setVisible(true);
+            w_pad.btn5.setVisible(true);
+            w_pad.btn4.setVisible(true);
+            w_pad.btn3.setVisible(true);
+            w_pad.btn2.setVisible(true);
+            w_pad.btn1.setVisible(true);
+            w_pad.ok_btn.setVisible(true);
+            w_pad.point_btn.setVisible(true);
+            w_pad.del_btn.setVisible(true);
+
+            pad.btn0.setVisible(true);
+            pad.btn9.setVisible(true);
+            pad.btn8.setVisible(true);
+            pad.btn7.setVisible(true);
+            pad.btn6.setVisible(true);
+            pad.btn5.setVisible(true);
+            pad.btn4.setVisible(true);
+            pad.btn3.setVisible(true);
+            pad.btn2.setVisible(true);
+            pad.btn1.setVisible(true);
+            pad.ok_btn.setVisible(true);
+            pad.point_btn.setVisible(true);
+            pad.del_btn.setVisible(true);
+
         }
         if(e.getSource() == quit) {
             dispose();
         }
         if(e.getSource() == deposit) {
             pad.setVisible(true);
+        }
+        if(e.getSource() == withdraw) {
+            w_pad.setVisible(true);
+        }
+        if(e.getSource() == prev_transac_button) {
+
+            if(bal.prev_transac > 0) {
+                    lt_status.setText("Deposited: Php " + String.format("%.2f", bal.prev_transac));
+            }else if(bal.prev_transac < 0) {
+                    lt_status.setText("Withdrawn: Php " + String.format("%.2f", -bal.prev_transac));
+            }else {
+                    lt_status.setText("No transactions done.");
+            }
+            lt_panel.setVisible(true);
         }
     }
 }
@@ -172,13 +249,30 @@ class keypad extends JPanel implements ActionListener{
     JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8 , btn9, btn0, point_btn, ok_btn;
     JButton yes_btn, no_btn, del_btn;
     JTextField deposit_field;
+    JPanel error_msg;
     JLabel confirm;
+    String type;
     main_buttons main;
+    boolean one_point = false;
 
-    public keypad(main_buttons main) {
+    public keypad(main_buttons main, String type) {
         this.setLayout(null);
         this.setBounds(250, 100, 210, 300);
         this.main = main;
+        this.type = type;
+
+        // error message for greater withdrawal amount than balance
+        error_msg = new JPanel();
+        error_msg.setBounds(0, 120, 210, 60);
+        error_msg.setVisible(false);
+        JLabel err_text1 = new JLabel();
+        JLabel err_text2 = new JLabel();
+        err_text1.setText("Error: The amount is");
+        err_text1.setBounds(0, 0, 210, 30);
+        err_text2.setText("higher than the balance.");
+        err_text2.setBounds(0, 30, 210, 30);
+        error_msg.add(err_text1);
+        error_msg.add(err_text2);
 
         // make a text field for the input
         deposit_field = new JTextField();
@@ -276,6 +370,7 @@ class keypad extends JPanel implements ActionListener{
         this.add(no_btn);
         this.add(confirm);
         this.add(del_btn);
+        this.add(error_msg);
 
     }
 
@@ -309,10 +404,22 @@ class keypad extends JPanel implements ActionListener{
             field_text += "9";
         }
         if(e.getSource() == btn0) {
-            field_text += "0";
+            if(field_text.equals("")) {
+                    field_text += "0";
+            }
+            else if(field_text.substring(0,1).equals("0")) {
+                    if(field_text.length() > 1 && field_text.substring(1,2).equals(".")) {
+                            field_text += "0";
+                    }
+            }else {
+                    field_text += "0";
+            }
         }
         if(e.getSource() == point_btn) {
-            field_text += ".";
+            if(one_point == false && !field_text.equals("")) {
+                field_text += ".";
+                one_point = true;
+            }
         }
         if(e.getSource() == ok_btn) {
             btn0.setVisible(false);
@@ -328,37 +435,66 @@ class keypad extends JPanel implements ActionListener{
             ok_btn.setVisible(false);
             point_btn.setVisible(false);
             del_btn.setVisible(false);
+            main.setVisible(false);
 
-            yes_btn.setVisible(true);
-            no_btn.setVisible(true);
-            confirm.setVisible(true);
+            Double amt = Double.parseDouble(field_text);
+
+            if(type.equals("withdraw")) {
+                    if(amt <= bal.amt) {
+                            yes_btn.setVisible(true);
+                            no_btn.setVisible(true);
+                            confirm.setVisible(true);
+                    }else{
+                            error_msg.setVisible(true);
+                            main.setVisible(true);
+                    }
+            }else {
+                    yes_btn.setVisible(true);
+                    no_btn.setVisible(true);
+                    confirm.setVisible(true);
+            }
         }
         if(e.getSource() == del_btn) {
-            field_text = field_text.substring(0, field_text.length()-1);
+            if(!field_text.equals("")) {
+                if(field_text.substring(field_text.length()-1).equals(".")) {
+                        one_point = false;
+                }
+                field_text = field_text.substring(0, field_text.length()-1);
+            }else {
+                field_text = "";
+            }
         }
         if(e.getSource() == yes_btn) {
-            Double amt = Double.parseDouble(field_text);
-            bal.amt += amt;
-            System.out.println(bal.amt);
 
-            btn0.setVisible(true);
-            btn9.setVisible(true);
-            btn8.setVisible(true);
-            btn7.setVisible(true);
-            btn6.setVisible(true);
-            btn5.setVisible(true);
-            btn4.setVisible(true);
-            btn3.setVisible(true);
-            btn2.setVisible(true);
-            btn1.setVisible(true);
-            ok_btn.setVisible(true);
-            point_btn.setVisible(true);
-            del_btn.setVisible(true);
-
+//			btn0.setVisible(true);
+//			btn9.setVisible(true);
+//			btn8.setVisible(true);
+//			btn7.setVisible(true);
+//			btn6.setVisible(true);
+//			btn5.setVisible(true);
+//			btn4.setVisible(true);
+//			btn3.setVisible(true);
+//			btn2.setVisible(true);
+//			btn1.setVisible(true);
+//			ok_btn.setVisible(true);
+//			point_btn.setVisible(true);
+//			del_btn.setVisible(true);
+//			
             yes_btn.setVisible(false);
             no_btn.setVisible(false);
             confirm.setVisible(false);
 
+            Double amt = Double.parseDouble(field_text);
+
+            if(type.equals("deposit")) {
+                    bal.amt += amt;
+                    bal.prev_transac = amt;
+            }else if(type.equals("withdraw")) {
+                    bal.amt -= amt;
+                    bal.prev_transac = -amt;
+            }
+
+            one_point = false;
             main.doClick();
         }
         if(e.getSource() == no_btn) {
@@ -375,6 +511,7 @@ class keypad extends JPanel implements ActionListener{
             ok_btn.setVisible(true);
             point_btn.setVisible(true);
             del_btn.setVisible(true);
+            main.setVisible(true); //checkpoint
 
             yes_btn.setVisible(false);
             no_btn.setVisible(false);
@@ -389,6 +526,7 @@ class keypad extends JPanel implements ActionListener{
 class bal{
 	
 	public static Double amt = 0.0;
+	public static Double prev_transac = 0.0;
 
 }
 
