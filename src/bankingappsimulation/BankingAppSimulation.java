8275462452a86ccd2c019f9e579eb6ff9a245464
John Isaac Delgado <ji23delgado@gmail.com>
2021-01-22 22:46:28 +0800

    idk what i changed

--- src/bankingappsimulation/BankingAppSimulation.java
+++ src/bankingappsimulation/BankingAppSimulation.java
@@ -1,22 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package bankingappsimulation;
 import java.util.Scanner;
-import java.lang.Math;
 /**
  *
  * @author ISAAC
  */
 public class BankingAppSimulation {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         bankGUI gui = new bankGUI();
+        gui.setTitle("Test modify");
     }
     
 }
